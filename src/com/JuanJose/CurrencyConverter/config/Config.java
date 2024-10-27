bc820ab2b64bc23419f28d6d7cf841847f29353e
JuanJoseVillamizar <juanjvs1419@gmail.com>
2024-10-27 11:01:37 -0500

    Moved API key to enviroment variables and created config package to read them

---
+++ src/com/JuanJose/CurrencyConverter/config/Config.java
@@ -0,0 +1,34 @@
+package com.JuanJose.CurrencyConverter.config;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Config {
+    private static final String ENV_FILE = ".env";
+    private Map<String, String> propertis = new HashMap<>();
+
+    public Config() {
+        loadEnv();
+    }
+
+    private void loadEnv() {
+        try (BufferedReader br = new BufferedReader(new FileReader(ENV_FILE))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split("=");
+                if (parts.length == 2) {
+                    propertis.put(parts[0].trim(), parts[1].trim());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public String get(String key) {
+        return propertis.get(key);
+    }
+}
+
--- src/com/JuanJose/CurrencyConverter/exchange/ExchangeService.java
+++ src/com/JuanJose/CurrencyConverter/exchange/ExchangeService.java
@@ -1,5 +1,6 @@
 package com.JuanJose.CurrencyConverter.exchange;
 
+import com.JuanJose.CurrencyConverter.config.Config;
 import com.JuanJose.CurrencyConverter.models.Rates;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -11,9 +12,15 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ExchangeService {
-    private final String API_KEY = "4d3a19754be030e46d5505e8";
+    private final String API_KEY;
+    public ExchangeService(){
+        Config config = new Config();
+        API_KEY = config.get("EXCHANGE_API_KEY");
+    }
     public Rates fetchRates(String baseCurrent,String targetCurrent,double amount) throws IOException, InterruptedException {
+        System.out.println(API_KEY);
         String URL_API = "https://v6.exchangerate-api.com/v6/"+API_KEY+"/pair/"+baseCurrent+"/"+targetCurrent +"/"+amount;
+        System.out.println(URL_API);
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(URL_API))
